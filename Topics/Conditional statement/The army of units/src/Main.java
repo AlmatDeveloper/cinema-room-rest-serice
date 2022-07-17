3b28b6324936265927ca2b54e8d83b309790ea75
Almat <almatmelis8@gmail.com>
2022-07-17 19:18:11 +0600

    commit

--- Topics/Conditional statement/The army of units/src/Main.java
+++ Topics/Conditional statement/The army of units/src/Main.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        
+        int unitsNum = scanner.nextInt();
+        
+        if (unitsNum < 1) {
+            System.out.print("no army");
+        } else if (unitsNum > 0 && unitsNum < 20) {
+            System.out.print("pack");
+        } else if (unitsNum > 19 && unitsNum < 250) {
+            System.out.print("throng");
+        } else if (unitsNum > 249 && unitsNum < 1000) {
+            System.out.print("zounds");
+        } else if (unitsNum > 999) {
+            System.out.print("legion");
+        }
     }
 }
